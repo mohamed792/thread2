c0f5c58b250b0261ba58ad91508cd94e8b5d63f8
mohamed elsayed <mohamedalsaid370@gmail.com>
2024-10-11 12:27:43 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+ public class Main {
+
+    public static void main(String[] args) {
+
+        Double doubleValue = Double.parseDouble("1.0");
+        Integer intValue = doubleValue.intValue();
+        System.out.println(intValue);
+    }
+}
